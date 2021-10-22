1729686958e16fc6a23d3d5c9a38d5d10535aaa1
Neagu Andrei-Alexandru <58010312+Neagu-Andrei@users.noreply.github.com>
2021-10-22 22:57:25 +0300

    Initial commit

---
+++ Driver.py
@@ -0,0 +1,32 @@
+from selenium.webdriver.support import expected_conditions as EC
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+
+
+class DriverController:
+    ytLink = "https://www.youtube.com/"
+    agreeButtonXPath = "/html/body/ytd-app/ytd-consent-bump-v2-lightbox/tp-yt-paper-dialog/div[2]/div[2]/div[""5]/div[2]/ytd-button-renderer[2]/a/tp-yt-paper-button/yt-formatted-string"
+
+    def __init__(self):
+        self.driver = webdriver.Chrome()
+        self.driver.get(self.ytLink)
+        self.driver.implicitly_wait(10)
+        WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.XPATH, self.agreeButtonXPath))).click()
+
+    def select_video(self, path, number):
+        videos = self.driver.find_elements(By.XPATH, path)
+        WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable(videos[number])).click()
+
+    def search_for_video(self, name):
+        searchbar = WebDriverWait(self.driver, 10).until(
+                 (EC.visibility_of_element_located((By.CSS_SELECTOR, "input#search"))))
+        searchbar.click()
+        searchbar.send_keys(name)
+        WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//*[@id='search-icon-legacy']"))).click()
+        self.driver.implicitly_wait(10)
+        self.select_video("//*[@id='contents']/ytd-video-renderer[1]", 0)
+
+
+
+# //*[@id='content']/ytd-rich-grid-media -> main page
